dc31ff5e03cb0c977d50bb5a1171ab303d246073
Peter Shaw <pshaw@voltdb.com>
2015-07-29 14:32:48 -0400

    ENG-8695 save

--- tools/copy-lib.sh
+++ tools/copy-lib.sh
@@ -15,19 +15,22 @@ echo '$1 is' $1
 echo '$2 is' $2
 echo 'lib is' obj/release/nativelibs/libvoltdb*
 
+
 OS=`uname`
 
 if [ "$OS" = "Darwin" -a -e obj/release/nativelibs/libvoltdb*.jnilib ]
 then
     # the Mac case...
     tar cf - obj/release/nativelibs/ | ssh volt0 "cd libs; tar xf -"
+    exit 0
 else
     # the Linux case...
     if [ -e ~/libs/obj/release/nativelibs/* ]; then
         mkdir -p $1
         cp ~/libs/obj/release/nativelibs/* $1
+        exit 0
     else
         echo "++++++++++++++nativelibs not found!"
+        exit 0
     fi
 fi
-exit 0
